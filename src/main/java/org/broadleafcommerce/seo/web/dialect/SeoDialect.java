ec3c21a325496cbeb28fdac2bf6a334803f3c123
Andre Azzolini <apazzolini@gmail.com>
2013-01-04 17:09:51 -0600

    Use extension paradigm for module application contexts

--- src/main/java/org/broadleafcommerce/seo/web/dialect/SeoDialect.java
+++ src/main/java/org/broadleafcommerce/seo/web/dialect/SeoDialect.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.broadleafcommerce.core.web.dialect;
+package org.broadleafcommerce.seo.web.dialect;
 
 import org.thymeleaf.dialect.AbstractDialect;
 import org.thymeleaf.processor.IProcessor;
