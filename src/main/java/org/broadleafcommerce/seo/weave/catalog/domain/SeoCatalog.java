298c965b826794c878b1b0811626ca6a514753f1
Justin Munn <munnja001@gmail.com>
2013-03-05 13:22:44 -0500

    added null check for delegated methods to prevent null pointer exceptions

--- src/main/java/org/broadleafcommerce/seo/weave/catalog/domain/SeoCatalog.java
+++ src/main/java/org/broadleafcommerce/seo/weave/catalog/domain/SeoCatalog.java
@@ -1,10 +1,10 @@
 package org.broadleafcommerce.seo.weave.catalog.domain;
 
-import org.broadleafcommerce.seo.domain.catalog.SeoMetaData;
-import org.broadleafcommerce.seo.domain.catalog.SeoMetaDataImpl;
-
 import javax.annotation.Nullable;
 import javax.persistence.Embedded;
+
+import org.broadleafcommerce.seo.domain.catalog.SeoMetaData;
+import org.broadleafcommerce.seo.domain.catalog.SeoMetaDataImpl;
 
 /**
  * The template class that will be weaved in via JPA class transformation at runtime.
@@ -15,49 +15,56 @@ public class SeoCatalog implements SeoMetaData {
 
     @Embedded
     protected SeoMetaDataImpl embeddableSeoMetaData = new SeoMetaDataImpl();
+    
+    protected SeoMetaData getEmbeddableSeoMetaData() {
+    	if (embeddableSeoMetaData == null) {
+    		embeddableSeoMetaData = new SeoMetaDataImpl();
+    	}
+    	return embeddableSeoMetaData;
+    }
 
     @Override
     @Nullable
     public String getMetaDescription() {
-        return embeddableSeoMetaData.getMetaDescription();
+        return getEmbeddableSeoMetaData().getMetaDescription();
     }
 
     @Override
     public void setMetaDescription(@Nullable String metaDescription) {
-        embeddableSeoMetaData.setMetaDescription(metaDescription);
+    	getEmbeddableSeoMetaData().setMetaDescription(metaDescription);
     }
 
     @Override
     @Nullable
     public String getMetaKeywords() {
-        return embeddableSeoMetaData.getMetaKeywords();
+        return getEmbeddableSeoMetaData().getMetaKeywords();
     }
 
     @Override
     public void setMetaKeywords(@Nullable String metaKeywords) {
-        embeddableSeoMetaData.setMetaKeywords(metaKeywords);
+    	getEmbeddableSeoMetaData().setMetaKeywords(metaKeywords);
     }
 
     @Override
     @Nullable
     public String getMetaRobot() {
-        return embeddableSeoMetaData.getMetaRobot();
+        return getEmbeddableSeoMetaData().getMetaRobot();
     }
 
     @Override
     public void setMetaRobot(@Nullable String metaRobot) {
-        embeddableSeoMetaData.setMetaRobot(metaRobot);
+    	getEmbeddableSeoMetaData().setMetaRobot(metaRobot);
     }
 
     @Override
     @Nullable
     public String getTitleFragment() {
-        return embeddableSeoMetaData.getTitleFragment();
+        return getEmbeddableSeoMetaData().getTitleFragment();
     }
 
     @Override
     public void setTitleFragment(@Nullable String titleFragment) {
-        embeddableSeoMetaData.setTitleFragment(titleFragment);
+    	getEmbeddableSeoMetaData().setTitleFragment(titleFragment);
     }
 
 }
